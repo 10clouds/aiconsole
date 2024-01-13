ffd4d5d1d269cbc3f2139f15e91a1c3c65d7b6f5
vlad <vlad@malinovski.org>
2024-01-13 13:51:12 +0100

    * refactor(users/models.py): replace @validator with @model_validator for username default setting

--- backend/aiconsole/core/users/models.py
+++ backend/aiconsole/core/users/models.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from pydantic import BaseModel, EmailStr, HttpUrl, validator
+from pydantic import BaseModel, EmailStr, HttpUrl, model_validator
 
 DEFAULT_USERNAME = "user"
 
@@ -18,11 +18,12 @@ class UserProfile(BaseModel):
     avatar_url: Optional[HttpUrl | str] = None
     gravatar: bool = False
 
-    @validator("username", pre=True, always=True)
-    def set_default_username(cls, v: Optional[str], values: dict) -> str:
-        if v is None:
-            email = values.get("email")
-            if isinstance(email, str):
-                return email.split("@")[0]
-            return DEFAULT_USERNAME
-        return v
+    @model_validator(mode="after")
+    def set_default_username(self):
+        if self.username is None:
+            email = self.email
+            if email:
+                self.username = email.split("@")[0]
+            else:
+                self.username = DEFAULT_USERNAME
+        return self
