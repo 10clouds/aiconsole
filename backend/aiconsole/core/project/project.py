d657d511bd358b9eec861eeef2a92535242f9286
Maciej Cielecki <maciej.cielecki@10clouds.com>
2024-03-10 07:24:09 +0100

    Update project.py

--- backend/aiconsole/core/project/project.py
+++ backend/aiconsole/core/project/project.py
@@ -14,9 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from datetime import datetime
 import os
 import sys
+from datetime import datetime
 from pathlib import Path
 from typing import TYPE_CHECKING
 
